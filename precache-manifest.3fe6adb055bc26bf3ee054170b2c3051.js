a2d88d69a23ed0d36812636e36e543f320dfd06f
nhat tri <nhattri1503@gmail.com>
2020-06-02 19:50:58 +0700

    first commit

---
+++ precache-manifest.3fe6adb055bc26bf3ee054170b2c3051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c0e6b99bdfe146e0ab8123f33f766e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf053266cb98db1ea261",
+    "url": "/static/css/2.6aa8c7b6.chunk.css"
+  },
+  {
+    "revision": "1e2169dda979c2ede652",
+    "url": "/static/css/main.04d6a3dc.chunk.css"
+  },
+  {
+    "revision": "bf053266cb98db1ea261",
+    "url": "/static/js/2.14e33482.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.14e33482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2169dda979c2ede652",
+    "url": "/static/js/main.a8488dcd.chunk.js"
+  },
+  {
+    "revision": "de8f34af5d44ac688fa1",
+    "url": "/static/js/runtime-main.710dcb79.js"
+  }
+]);
